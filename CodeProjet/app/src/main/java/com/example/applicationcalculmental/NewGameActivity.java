dd675ef513ca74136d0b9bf6e859e763a6ec747e
RMFireSteel <66364083+RMFireSteel@users.noreply.github.com>
2022-03-17 00:23:57 +0100

    Démarrage Partie fonctionnelle

--- CodeProjet/app/src/main/java/com/example/applicationcalculmental/NewGameActivity.java
+++ CodeProjet/app/src/main/java/com/example/applicationcalculmental/NewGameActivity.java
@@ -2,12 +2,44 @@ package com.example.applicationcalculmental;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewGameActivity extends AppCompatActivity {
+
+    private Button easy, medium, hard, go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
+
+        easy = (Button) findViewById(R.id.easy);
+        easy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Facile");
+            }
+        });
+
+        medium = (Button) findViewById(R.id.medium);
+        medium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Moyen");
+            }
+        });
+
+        hard = (Button) findViewById(R.id.hard);
+        hard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Difficile");
+            }
+        });
     }
+
 }
